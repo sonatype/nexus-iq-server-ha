11723ebbfeee41a694a46dc35d9e1f678a20c5b2
kasun ranawaka <kranawaka@sonatype.com>
2023-01-13 11:31:09 +0000

    Add chart release scripts - CLM-23674

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Copyright (c) 2020-present Sonatype, Inc. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+#
+
+helm plugin install https://github.com/quintush/helm-unittest
+
+set -e
+
+# lint yaml of chart
+helm lint chart
+
+# unit test
+(cd ./chart; helm unittest -3 -t junit -o test-output.xml .)
+
+# package the charts into tgz archives
+helm package chart --destination docs
